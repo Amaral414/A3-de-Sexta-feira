166037c819b1cdb9f40b88ba28a8b287af735c9e
philgodoy96 <168253578+philgodoy96@users.noreply.github.com>
2024-05-05 00:13:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,64 @@ let seats = document.getElementById("seats");
 let selectedSeats = [];
 let purchasedSeats = [];
 let total = 0;
+let priorDate;
 let date;
 let time;
 
 const logan = {
-    session1: [[], "05/06", "13:00"],
-    session2: [[], "05/06", "13:00"],
-    session3: [[], "05/06", "13:00"],
-
+    session1: [["A1", "A2", "A3", "A4"], "13/06", "11:00"],
+    session2: [["B1", "B2", "B3", "B4"], "13/06", "14:00"],
+    session3: [["C1", "C2", "C3", "C4"], "13/06", "17:00"],
+    session3: [["D1", "D2", "D3", "D4"], "13/06", "20:00"],
 }
+
 purchase();
 totalPrice.textContent = `R$ ${total},00`
 
-function pickSession(value){
-    selectedSeats = value;
-    purchase();
+function displaySessions(selectedDate){
+    if(priorDate != null && priorDate != selectedDate){
+        document.getElementById(priorDate).style.display = "none";
+    }
+
+    date = document.getElementById(selectedDate);
+    if(date.style.display == "none"){
+        date.style.display = "block";
+        priorDate = selectedDate;
+    }
+    
+    else{
+        date.style.display = "none";
+        priorDate = selectedDate;
+    }
+
 }
 
-function displayChoiceArea(){
-    const choiceArea = document.getElementById("choice-area");
+function displayDateArea(){
+    const dateArea = document.getElementById("date-area");
     const overlay = document.getElementById("overlay");
-    choiceArea.style.display = "block";
+    dateArea.style.display = "block";
     overlay.style.display = "block";
 }
 
+function closeDateArea(){
+    const dateArea = document.getElementById("date-area");
+    const overlay = document.getElementById("overlay");
+    dateArea.style.display = "none";
+    overlay.style.display = "none";
+}
+
+function displayChoiceArea(value){
+    const dateArea = document.getElementById("date-area");
+    const choiceArea = document.getElementById("choice-area")
+    dateArea.style.display = "none";
+    choiceArea.style.display = "block";
+    selectedSeats = value[0];
+    purchase();
+}
+
 function closeChoiceArea(){
-    const choiceArea = document.getElementById("choice-area");
+    const choiceArea = document.getElementById("choice-area")
+    const overlay = document.getElementById("overlay");
     choiceArea.style.display = "none";
     overlay.style.display = "none";
 }
@@ -45,7 +77,6 @@ function clickSeat(seat){
         if (selectedSeats.includes(seat.id) != true){
             selectedSeat.style.backgroundColor = "#8B008B";
             selectedSeats.push(selectedSeat.id);
-            console.log(selectedSeats);
             seats.textContent = selectedSeats.join(" ");
             total += 30;
             totalPrice.textContent = `R$ ${total},00`
@@ -55,7 +86,6 @@ function clickSeat(seat){
         else{
             selectedSeat.style.backgroundColor = "rgb(24, 201, 5)";
             selectedSeats = selectedSeats.filter((chosenSeat) => chosenSeat != seat.id)
-            console.log(selectedSeats);
             seats.textContent = selectedSeats.join(" ");
             total -= 30;
             totalPrice.textContent = `R$ ${total},00`
@@ -69,7 +99,6 @@ function purchase(){
         boughtSeat = document.getElementById(boughtSeat);
         boughtSeat.style.backgroundColor = "red";
         boughtSeat.classList.add("purchased")
-        console.log(boughtSeat.classList)
         purchasedSeats.push(boughtSeat.id);
         selectedSeats = selectedSeats.filter((seats) => seats != boughtSeat.id)
     }
