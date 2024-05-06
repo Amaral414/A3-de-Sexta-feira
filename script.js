2bc411eb5df4f1801b8a3e4b815150eaafbc530c
philgodoy96 <168253578+philgodoy96@users.noreply.github.com>
2024-05-05 21:09:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ let date;
 let time;
 
 const logan = {
-    session1: [["A1", "A2", "A3", "A4"]/*purchased seats*/, []/*selected seats*/, "13/06", "11:00"],
-    session2: [["B1", "B2", "B3", "B4"]/*purchased seats*/, []/*selected seats*/, "13/06", "14:00"],
-    session3: [["C1", "C2", "C3", "C4"]/*purchased seats*/, []/*selected seats*/, "13/06", "17:00"],
-    session4: [["D1", "D2", "D3", "D4"]/*purchased seats*/, []/*selected seats*/, "13/06", "20:00"],
+    session1: [["A1", "A2", "A3", "A4"]/*purchased seats*/, []/*selected seats*/, "13/06", "15:00"],
+    session2: [["B1", "B2", "B3", "B4"]/*purchased seats*/, []/*selected seats*/, "13/06", "20:00"],
+    session3: [["C1", "C2", "C3", "C4"]/*purchased seats*/, []/*selected seats*/, "14/06", "14:00"],
+    session4: [["D1", "D2", "D3", "D4"]/*purchased seats*/, []/*selected seats*/, "14/06", "18:00"],
+    session5: [["E1", "E2", "E3", "E4"]/*purchased seats*/, []/*selected seats*/, "14/06", "21:00"],
+    session6: [["F1", "F2", "F3", "F4"]/*purchased seats*/, []/*selected seats*/, "15/06", "11:00"],
+    session7: [["G1", "G2", "G3", "G4"]/*purchased seats*/, []/*selected seats*/, "15/06", "15:00"],
+    session8: [["H1", "H2", "H3", "H4"]/*purchased seats*/, []/*selected seats*/, "15/06", "20:00"],
+    session9: [["I1", "I2", "I3", "I4"]/*purchased seats*/, []/*selected seats*/, "16/06", "11:00"],
+    session10: [["J1", "J2", "J3", "J4"]/*purchased seats*/, []/*selected seats*/, "16/06", "15:00"],
+    session11: [["A13", "A14", "A15", "A16"]/*purchased seats*/, []/*selected seats*/, "16/06", "19:00"],
+    session12: [["B17", "B18", "B19", "B20"]/*purchased seats*/, []/*selected seats*/, "17/06", "15:00"],
+    session13: [["C17", "C18", "C19", "C20"]/*purchased seats*/, []/*selected seats*/, "17/06", "20:00"],
+    session14: [["D17", "D18", "D19", "D20"]/*purchased seats*/, []/*selected seats*/, "18/06", "20:00"],
+    session15: [["E17", "E18", "E19", "E20"]/*purchased seats*/, []/*selected seats*/, "19/06", "14:00"],
+    session16: [["F17", "F18", "F19", "F20"]/*purchased seats*/, []/*selected seats*/, "19/06", "20:00"],
 }
+
 totalPrice.textContent = `R$ ${total},00`
 
 function displaySessions(selectedDate){
@@ -48,6 +61,8 @@ function closeDateArea(){
 
 function displayChoiceArea(value){
     session = value;
+    date = session[2];
+    time = session[3];
 
     for(let i of document.getElementsByClassName("seat")){
         i.style.backgroundColor = "rgb(24, 201, 5)";
